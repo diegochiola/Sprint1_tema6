c1caacb08ec225df7e47da70e31584a06efc53af
diega <diegochiola@gmail.com>
2023-12-30 12:21:06 +0100

    ejercicio2

--- Ejercicio2/formulario.php
+++ Ejercicio2/formulario.php
@@ -1,16 +1,17 @@
 <?php
-//variables para que se guarden los datos del input del formulario
-$nombre = $_POST['name'];
-$usuario =$_POST['usuario'];
-$email =$_POST['email'];
-$textArea =$_POST['textArea'];
+//se inicia sesion
+session_start();
 
-//imprimir por pantalla al dar al boton submit
-/*
-echo "Su nombre es: " . $nombre . ", el usuario es: " . $usuario . " <br>". 
-    " El email ingresado es: ". $email . "  <br>".
-    " El mensaje que $nombre dejo es: ". $textArea;
-*/
+//variables para que se guarden los datos del input del formulario
+if($_SERVER["REQUEST_METOD"] == "POST"){
+    $nombre = $_POST['name'];
+    $usuario =$_POST['usuario'];
+    $email =$_POST['email'];
+    $textArea =$_POST['textArea'];
+}
+//Guardo el nombre del usuario en la sesion
+$_SESSION["usuario"] = $usuario;
+
 //creo variable mensaje que incluya el mensaje que se imprimira por pantalla
 $mensaje = "El formulario fue rellenado por: ".$nombre . " <br>" .
     "Con usuario: " . $usuario."  <br>".
@@ -20,5 +21,5 @@ echo $mensaje;
 
 //Redireccion cuando se rellena el form
 //header('Location:mensaje.html');
-
+exit();
 ?>
