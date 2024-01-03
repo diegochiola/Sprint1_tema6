6336a9f48705856d8d57e32541fdabd2a6b97d3e
diega <diegochiola@gmail.com>
2024-01-03 11:13:43 +0100

    correcciones en el ejercicio

--- Ejercicio2/formulario.php
+++ Ejercicio2/formulario.php
@@ -3,22 +3,31 @@
 session_start();
 
 //variables para que se guarden los datos del input del formulario
-if($_SERVER["REQUEST_METOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     $usuario =$_POST['usuario'];
     $password =$_POST['password'];
     $email =$_POST['email'];
     
 }
 //Guardo el nombre del usuario en la sesion
-$_SESSION["usuario"] = $usuario;
-
-//creo variable mensaje que incluya el mensaje que se imprimira por pantalla
-$mensaje = "La sesion fue iniciada por el usuario: ".$usuario . " <br>" .
-    "Con el password: " . $password."  <br>".
-    "El email: ".$email . " <br>";
-echo $mensaje;
+/*
+$_SESSION['usuario'] = $usuario;
+$_SESSION['password'] = $password;
+$_SESSION['email'] = $email;
+*/
+$arrayDatos = array('usuario' => $usuario, 'password' => $password, 'email'=> $email);
+$_SESSION['datos']= $arrayDatos;
+echo "Gracias por iniciar sesion";
+echo'<pre>';
+var_dump($_SESSION['datos']) ;
+echo '</pre>';
 
 //Redireccion cuando se rellena el form
 //header('Location:mensaje.html');
-exit();
+
+
+//Destruir la sesion despues de mostrar datos de sesion por pantalla
+session_destroy();
+exit;
+
 ?>
